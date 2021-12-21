1a0523ef67cc354fda83d64053b25bb49f5bef0f
DovIra <44786442+DovIra@users.noreply.github.com>
2021-12-21 04:09:59 +0200

    Add files via upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter)
 import App from './views/App'
 import ProductCatalog from './views/ProductCatalog'
 import ProductView from './views/ProductView'
+import Survey from './views/Survey'
 import NotFound from './views/NotFound'
 
 const router = new VueRouter({
@@ -17,6 +18,10 @@ const router = new VueRouter({
         {path: '/product/:id/', name: 'product.view',component: ProductView
         },
         {path: '/products/category/:catalog_id', name: 'products.catalog',component: ProductCatalog
+        },
+        {path: '/cart', name: 'products.catalog',component: ProductCatalog
+        },,
+        {path: '/survey', name: 'survey',component: Survey
         },
         { path: '/404', name: '404', component: NotFound },
     ],
